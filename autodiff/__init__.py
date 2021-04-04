e7ed92926279145ddb39d9cfe1069673a207bb30
Annika Brundyn <annikabrundyn1@gmail.com>
2021-04-04 00:49:14 -0400

    forward grad

---
+++ autodiff/__init__.py
@@ -0,0 +1,2 @@
+from autodiff.nodes import *
+from autodiff.grad import *
--- autodiff/nodes.py
+++ autodiff/nodes.py
@@ -1,37 +1,41 @@
-from .operations import *
 
 
 class Node(object):
 
     def __add__(self, other):
+        from .operations import Add
         return Add(self, other)
 
     def __sub__(self, other):
+        from .operations import Subtract
         return Subtract(self, other)
 
     def __mul__(self, other):
+        from .operations import Multiply
         return Multiply(self, other)
 
     def __pow__(self, power, modulo=None):
+        from .operations import Power
         return Power(self, power)
 
     def __floordiv__(self, other):
+        from .operations import Divide
         return Divide(self, other)
 
     def __truediv__(self, other):
+        from .operations import Divide
         return Divide(self, other)
 
 
 class Variable(Node):
     def __init__(self, dtype, name=None):
         self.name = dtype if name is None else name
-        self.dtype = dtype
         self.value = None
         self.a = None
         self.b = None
 
     def __str__(self):
-        return 'Variable(name:{}, dtype: {})'.format(self.name, self.dtype)
+        return 'Variable(name:{})'.format(self.name)
 
     def forward_pass(self):
         return self.value
