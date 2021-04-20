5601548669796b3cd28d6898a54f97bcd1fc030b
Annika Brundyn <annikabrundyn1@gmail.com>
2021-04-20 12:29:46 -0400

    clean up scalar implementation

--- autodiff/utils.py
+++ autodiff/utils.py
@@ -1,12 +1,6 @@
 ### TODO: consider redoing this
 
 def reverse_topo_sort(top_node):
-    """
-    Returns a list of nodes in the top_node subtree, sorted in the reverse topological order.
-
-    :param top_node:
-    :return: nodes iterator
-    """
 
     def topo_sort_dfs(node, visited, topo_sort):
         if node in visited:
