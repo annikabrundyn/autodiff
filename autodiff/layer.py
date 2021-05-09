5bd449ccf05e1ada629c5b0c9a88cb9c06b0ebfb
Annika Brundyn <annikabrundyn1@gmail.com>
2021-05-09 18:19:24 -0400

    google docstring style

--- autodiff/layer.py
+++ autodiff/layer.py
@@ -4,7 +4,7 @@ from typing import Tuple
 
 
 class Layer(ABC):
-    """abstract class of Layer"""
+    """abstract layer class"""
 
     def __init__(self, layer_type: str, output_dim: int):
         self.type = layer_type
@@ -27,56 +27,44 @@ class Layer(ABC):
 
 
 class Linear(Layer):
-    """Linear Layer.
-
-    Equivalent to Dense in Keras and to torch.nn.Linear in torch.
-
-    Parameters
-    ----------
-    input_dim : int
-        Number of input features of this layer.
-    output_dim : int
-        Number of output features of this layer.
+    """
+    Linear layer used in fully-connected network.
     """
 
     def __init__(self, input_dim: int, output_dim: int):
+        """
+        Args:
+            input_dim: number of  dimensions in the input
+            output_dim: number of dimensions in the output
+        """
         super().__init__('Linear', output_dim)
         self.weights = np.random.rand(output_dim, input_dim)
         self.biases = np.random.rand(output_dim, 1)
 
     def forward(self, input_val: np.ndarray) -> np.ndarray:
-        """Forward.
+        """Performs forward pass of this layer.
 
-        Performs forward propagation of this layer.
+        Args:
+            input_val: value being input to the layer
 
-        Parameters:
-        input_val : numpy.Array
-            Forward propagation of the previous layer.
-        Returns
-        -------
-        activation : numpy.Array
-            Forward propagation operation of the linear layer.
+        Returns:
+            weights * inputs + biases
+
         """
         self._prev_val = input_val
         return np.matmul(self.weights, self._prev_val) + self.biases
 
     def backward(self, dJ: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        """Backward.
+        """Back propagation of this layer.
 
-        Computes backward propagation pass of this layer.
+        Args:
+            dJ : gradient of the next layer.
 
-        Parameters
-        ----------
-        dJ : numpy.Array
-            Gradient of the next layer.
-        Returns
-        -------
-        delta : numpy.Array
-            Upcoming gradient, usually from an activation function.
-        dW : numpy.Array
-            Weights gradient of this layer.
-        dB : numpy.Array
-            Biases gradient of this layer.
+        Returns:
+            delta : upcoming gradient, usually from an activation function.
+            dW : weights gradient of this layer.
+            dB : biases gradient of this layer.
+
         """
         dW = np.dot(dJ, self._prev_val.T)
         dB = dJ.mean(axis=1, keepdims=True)
@@ -85,23 +73,20 @@ class Linear(Layer):
 
         return delta, dW, dB
 
-    def optimize(self, dW: np.ndarray, dB: np.ndarray, rate: float):
-        """Optimizes.
+    def optimize(self, dW: np.ndarray, dB: np.ndarray, learning_rate: float):
+        """Optimizes. Updates the weights according to gradient descent.
 
-        Performs the optimization of the parameters. For now,
-        optimization can only be performed using gradient descent.
+        Note:
+            For now, optimization can only be performed using gradient descent.
 
-        Parameters
-        ----------
-        dW : numpy.Array
-            Weights gradient.
-        dB : numpy.Array
-            Biases gradient.
-        rate: float
-            Learning rate of the gradient descent.
+        Args:
+            dW : Weights gradient.
+            dB : Biases gradient.
+            learning_rate: Learning rate of the gradient descent.
+
         """
-        self.weights = self.weights - rate * dW
-        self.biases = self.biases - rate * dB
+        self.weights = self.weights - learning_rate * dW
+        self.biases = self.biases - learning_rate * dB
 
 
 class ReLU(Layer):
